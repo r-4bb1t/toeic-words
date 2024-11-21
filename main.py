bb52fcc1b7a9f7c05a5aa8d8abb773e9c10911b8
r-4bb1t <r4bb1t.github@gmail.com>
2024-11-21 22:10:40 +0900

    🚀 Chore: Add dist files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtGui import QFontDatabase, QFont
 
 from widget import MaintWidget
@@ -18,7 +18,7 @@ def main():
         print("Font loading failed. Using default font.")
     app.setFont(QFont("Pretendard"))
 
-    chat_widget = MaintWidget("./words.csv")
+    chat_widget = MaintWidget(csv_path="./words.csv")
     chat_widget.setStyleSheet(
         "background-color: rgba(0, 0, 0, 0.4); border-radius: 30px;"
     )
